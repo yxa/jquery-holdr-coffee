3bf3ac93a6bd3b688ef8ce1e7cbbb227901ec95b
torbjorn josefsson <torbjorn.josefsson@gmail.com>
2012-05-08 14:56:06 +0200

    robohash and placholder kitten tests

--- spec/holdrSpec.coffee
+++ spec/holdrSpec.coffee
@@ -10,6 +10,43 @@ describe 'holdr jquery plugin, coffee version', ->
     ($ '#images').remove()
   
   it 'should be chainable', ->
-    images = ($ 'img');
+    images = ($ 'img')
     expect(images).toExist
     expect(images.holdr()).toBeTruthy
+
+  it 'should only replace images with empty src attributes', ->
+    images = ($ "img[src!='']")
+    expect(images.length).toBe 1
+    url = images.attr 'src'
+    allImages = ($ "img")
+    allImages.holdr()
+    expect($("img[src='#{url}']").length).toBe 1
+    
+  it 'should be able to replace more than one image with empty src attribute', ->
+    emptyImages = ($ "img[src='']")
+    expect(emptyImages.length).toBe 2
+    allImages = ($ "img")
+    expect(allImages.length).toBe 3
+    allImages.holdr()
+    emptyImages = ($ "img[src='']")
+    expect(emptyImages.length).toBe 0
+
+  it 'should be able to handle default or no placeholder site specified', ->
+    defaultProvider = 'http://flickholdr.com'
+    emptyImages = ($ "img[src='']")
+    emptyImages.holdr()
+    expect(emptyImages).toBeModified
+    emptyImages.each (index,item) ->
+      expect($(item).attr('src')).toEqual(defaultProvider + '/' + 200 + '/' + 300 + '/')
+
+
+  it 'should be able to handle placekitten as a placeholder site', ->
+    defaultProvider = 'http://placekitten.com'
+    emptyImages = ($ "img[src='']")
+    emptyImages.holdr({provider: 'placekitten'})
+    expect(emptyImages).toBeModified
+    emptyImages.each (index,item) ->
+      expect($(item).attr('src')).toEqual(defaultProvider + '/' + 200 + '/' + 300)
+
+
+  it 'should be able to handle robohash as a placeholder site', ->
--- spec/holdrSpec.js
+++ spec/holdrSpec.js
@@ -12,10 +12,52 @@ describe('holdr jquery plugin, coffee version', function() {
   afterEach(function() {
     return ($('#images')).remove();
   });
-  return it('should be chainable', function() {
+  it('should be chainable', function() {
     var images;
     images = $('img');
     expect(images).toExist;
     return expect(images.holdr()).toBeTruthy;
   });
+  it('should only replace images with empty src attributes', function() {
+    var allImages, images, url;
+    images = $("img[src!='']");
+    expect(images.length).toBe(1);
+    url = images.attr('src');
+    allImages = $("img");
+    allImages.holdr();
+    return expect($("img[src='" + url + "']").length).toBe(1);
+  });
+  it('should be able to replace more than one image with empty src attribute', function() {
+    var allImages, emptyImages;
+    emptyImages = $("img[src='']");
+    expect(emptyImages.length).toBe(2);
+    allImages = $("img");
+    expect(allImages.length).toBe(3);
+    allImages.holdr();
+    emptyImages = $("img[src='']");
+    return expect(emptyImages.length).toBe(0);
+  });
+  it('should be able to handle default or no placeholder site specified', function() {
+    var defaultProvider, emptyImages;
+    defaultProvider = 'http://flickholdr.com';
+    emptyImages = $("img[src='']");
+    emptyImages.holdr();
+    expect(emptyImages).toBeModified;
+    return emptyImages.each(function(index, item) {
+      return expect($(item).attr('src')).toEqual(defaultProvider + '/' + 200 + '/' + 300 + '/');
+    });
+  });
+  it('should be able to handle placekitten as a placeholder site', function() {
+    var defaultProvider, emptyImages;
+    defaultProvider = 'http://placekitten.com';
+    emptyImages = $("img[src='']");
+    emptyImages.holdr({
+      provider: 'placekitten'
+    });
+    expect(emptyImages).toBeModified;
+    return emptyImages.each(function(index, item) {
+      return expect($(item).attr('src')).toEqual(defaultProvider + '/' + 200 + '/' + 300);
+    });
+  });
+  return it('should be able to handle robohash as a placeholder site', function() {});
 });
